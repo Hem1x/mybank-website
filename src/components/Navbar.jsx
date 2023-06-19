0b155d37fd38d88f1a80027f98955764944c36a6
Sanek-2004 <iwanov.vany-ivanov@yandex.ru>
2023-06-20 00:20:10 +0300

    create the structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import {close, logo, menu} from '../assets'
+import {navLinks} from '../constants'
+
+const Navbar = () => {
+  return (
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+
+    </nav>
+  )
+}
+
+export default Navbar
