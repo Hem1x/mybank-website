f495a90fd70f00e67f0db139637582cfad0e6011
Sanek-2004 <iwanov.vany-ivanov@yandex.ru>
2023-06-20 12:58:07 +0300

    section "Business" is done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>Button</div>
+const Button = ({children, styles}) => (
+    <button type='button' className={`rounded-[15px] py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+      {children}
+    </button>
   )
-}
+
 
 export default Button
